e76cb2ddd9d7893e0c7d2b2b4431c9a1052a3163
purplebearddan <daniel_m@purplebeard.co.uk>
2023-08-29 19:46:35 +0100

    lesson complete

---
+++ app.js
@@ -0,0 +1,39 @@
+// Data Types and Built-in Methods and Properties
+
+
+const myString = "I'm a string"
+
+// console.log(myString)
+
+const myIntegerNumber = 23 // Number
+
+const myFloatNumber = 23.5 // Number
+
+
+
+const myBool = true //can be false
+
+
+const myIntegerArray = [1, 2, 3, 4, 5]
+
+
+
+// aka List
+const myStringArray = [
+    "doll",
+    "racecar",
+    "ball"
+]
+
+console.log(myStringArray[0].toUpperCase)
+
+// aka Dictionary
+const myObject = {
+    key: "value",
+    otherKey: "otherValue"
+}
+
+
+console.log(myObject.key)
+
+console.log(myObject.otherKey)
